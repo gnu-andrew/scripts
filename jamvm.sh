863f7c7e5bfe6e06d30ffda51305402c8ecd6328
Andrew John Hughes <gnu_andrew@member.fsf.org>
2008-05-19 22:07:33 +0100

    Add JamVM support.

---
+++ jamvm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. $HOME/projects/scripts/environment
+. $HOME/projects/scripts/functions
+
+if [ -e $JAMVM_HOME ]; then
+    cd $JAMVM_HOME;
+    make distclean;
+    cvs update -dP;
+else
+    cd `dirname $JAMVM_HOME`;
+    cvs -z3 -d:pserver:anonymous@cvs.jamvm.berlios.de:/cvsroot/jamvm co jamvm;
+    cd $JAMVM_HOME;
+fi
+./autogen.sh --help
+create_working_dir
+rm -rf jamvm &&
+mkdir jamvm &&
+cd jamvm &&
+$JAMVM_HOME/configure --with-classpath-install-dir=$CLASSPATH_INSTALL --prefix=$JAMVM_INSTALL
+make $MAKE_OPTS &> $JAMVM_HOME/errors && make install && echo DONE
+
