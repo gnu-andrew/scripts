5e9e96a4b808b734a6a9e5cd60c85757f27f0e6e
Andrew John Hughes <gnu_andrew@member.fsf.org>
2008-06-09 00:48:26 +0100

    Add make distcheck cleanup and zip file to IcedTea.

--- icedtea.sh
+++ icedtea.sh
@@ -8,6 +8,17 @@ JAVACFLAGS=
 JAVA_HOME=
 JDK_HOME=
 
+BUILD_DIR=${WORKING_DIR}/icedtea
+
+if test x$1 != "x"; then
+    if [ -e ${BUILD_DIR} ]; then
+	find ${BUILD_DIR} -type f -exec chmod 640 '{}' ';';
+	find ${BUILD_DIR} -type d -exec chmod 750 '{}' ';';
+	rm -rf ${BUILD_DIR};
+	mkdir ${BUILD_DIR};
+    fi
+fi
+
 if [ -e $ICEDTEA_HOME ]; then
     cd $ICEDTEA_HOME;
     hg pull -u;
@@ -19,9 +30,9 @@ else
 fi
 
 CONFIG_OPTS="--with-parallel-jobs=9 --with-libgcj-jar=/usr/lib/jvm/gcj-jdk-4.3/jre/lib/rt.jar \
-    --with-gcj-home=/usr/lib/jvm/gcj-jdk-4.3"
+    --with-gcj-home=/usr/lib/jvm/gcj-jdk-4.3 --with-openjdk-src-zip=${OPENJDK_ZIP}"
 
-cd $WORKING_DIR/icedtea &&
+cd ${BUILD_DIR} &&
 $ICEDTEA_HOME/configure ${CONFIG_OPTS}
 if test x$1 != "x"; then
     DISTCHECK_CONFIGURE_FLAGS=${CONFIG_OPTS} make distcheck;
