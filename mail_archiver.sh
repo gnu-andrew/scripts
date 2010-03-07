af4948590a1f916fe7e27050438b09845eab4cf9
Andrew John Hughes <gnu_andrew@member.fsf.org>
2010-03-07 17:02:42 +0000

    2010-03-07  Andrew John Hughes  <gnu_andrew@member.fsf.org>
    	* ChangeLog: Moved to...
    	* ChangeLog.old: this, ChangeLog can be generated
    	from Mercurial logs.
    	* mail_archiver.sh: New script to build the
    	mail archiver.

---
+++ mail_archiver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+. $HOME/projects/scripts/functions
+
+(if [ ! -e $ARCHIVER_HOME ]; then
+    cd `dirname $ARCHIVER_HOME`;
+    hg clone http://fuseyism.com/hg/mail-archiver;
+    cd $ARCHIVER_HOME;
+else
+    cd $ARCHIVER_HOME;
+    hg pull -u;
+fi
+./autogen.sh;
+create_working_dir
+rm -rf archiver
+mkdir archiver
+cd archiver &&
+$ARCHIVER_HOME/configure --prefix=$ARCHIVER_INSTALL &&
+make all install && echo DONE) 2>&1 | tee $ARCHIVER_HOME/errors
