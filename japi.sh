8f959bc219277adba173adc1f3480846742449e7
Andrew John Hughes <gnu_andrew@member.fsf.org>
2015-06-03 20:36:46 +0100

    Include IcedTea 3.x in JAPI runs.
    2015-06-01  Andrew John Hughes  <gnu_andrew@member.fsf.org>
    
    	* japi.sh: Add support for OpenJDK 8 / IcedTea 3.
    	Use system installations of IcedTea.

--- japi.sh
+++ japi.sh
@@ -10,17 +10,23 @@ fi
 
 # List of excluded packages is taken from jdk/make/docs/CORE_PKGS.gmk
 if test "x${REBUILD_ICEDTEA_JAPI}" = "xyes"; then
-  (${JAPI_HOME}/bin/japize as ${JAPI_INSTALL}/icedtea6 packages ${ICEDTEA6_INSTALL}/jre/lib/rt.jar +java \
-      $ICEDTEA6_INSTALL/jre/lib/jce.jar +javax +org -java.awt.dnd.peer -java.awt.peer -java.text.resources \
+  (${JAPI_HOME}/bin/japize as ${JAPI_INSTALL}/icedtea6 packages ${SYSTEM_ICEDTEA6}/jre/lib/rt.jar +java \
+      ${SYSTEM_ICEDTEA6}/jre/lib/jce.jar +javax +org -java.awt.dnd.peer -java.awt.peer -java.text.resources \
       -org.w3c.dom.css  -org.w3c.dom.html -org.w3c.dom.stylesheets -org.w3c.dom.traversal    \
       -org.w3c.dom.ranges -org.w3c.dom.views -org.omg.stub.javax.management.remote.rmi -org.classpath ; \
-  ${JAPI_HOME}/bin/japize as ${JAPI_INSTALL}/icedtea7 packages ${ICEDTEA7_INSTALL}/jre/lib/rt.jar +java \
-      $ICEDTEA7_INSTALL/jre/lib/jce.jar +javax +org -java.awt.dnd.peer -java.awt.peer -java.text.resources \
+  ${JAPI_HOME}/bin/japize as ${JAPI_INSTALL}/icedtea7 packages ${SYSTEM_ICEDTEA7}/jre/lib/rt.jar +java \
+      ${SYSTEM_ICEDTEA8}/jre/lib/jce.jar +javax +org -java.awt.dnd.peer -java.awt.peer -java.text.resources \
+      -org.w3c.dom.css  -org.w3c.dom.html -org.w3c.dom.stylesheets -org.w3c.dom.traversal    \
+      -org.w3c.dom.ranges -org.w3c.dom.views -org.omg.stub.javax.management.remote.rmi ; \
+  ${JAPI_HOME}/bin/japize as ${JAPI_INSTALL}/icedtea8 packages ${SYSTEM_ICEDTEA8}/jre/lib/rt.jar +java \
+      ${SYSTEM_ICEDTEA8}/jre/lib/jce.jar +javax +org -java.awt.dnd.peer -java.awt.peer -java.text.resources \
       -org.w3c.dom.css  -org.w3c.dom.html -org.w3c.dom.stylesheets -org.w3c.dom.traversal    \
       -org.w3c.dom.ranges -org.w3c.dom.views -org.omg.stub.javax.management.remote.rmi ; \
   ${JAPI_HOME}/bin/japize as ${JAPI_INSTALL}/javadoc6 packages ${ICEDTEA6_INSTALL}/jre/lib/rt.jar ${ICEDTEA6_INSTALL}/lib/tools.jar \
       +com.sun.javadoc +com.sun.tools.doclets -com.sun.tools.doclets.internal -com.sun.tools.doclets.formats -com.sun.tools.doclets.standard ; \
   ${JAPI_HOME}/bin/japize as ${JAPI_INSTALL}/javadoc7 packages ${ICEDTEA7_INSTALL}/jre/lib/rt.jar ${ICEDTEA7_INSTALL}/lib/tools.jar \
+      +com.sun.javadoc +com.sun.tools.doclets -com.sun.tools.doclets.internal -com.sun.tools.doclets.formats -com.sun.tools.doclets.standard ; \
+  ${JAPI_HOME}/bin/japize as ${JAPI_INSTALL}/javadoc8 packages ${ICEDTEA8_INSTALL}/jre/lib/rt.jar ${ICEDTEA8_INSTALL}/lib/tools.jar \
       +com.sun.javadoc +com.sun.tools.doclets -com.sun.tools.doclets.internal -com.sun.tools.doclets.formats -com.sun.tools.doclets.standard ; \
   ${JAPI_HOME}/bin/japicompat -v -j -o ${JAPI_INSTALL}/ignore-5-6.japio \
       ${JAPI_INSTALL}/ibmjdk15.japi.gz ${JAPI_INSTALL}/icedtea6.japi.gz ; \
@@ -30,12 +36,25 @@ if test "x${REBUILD_ICEDTEA_JAPI}" = "xyes"; then
       ${JAPI_INSTALL}/icedtea7.japi.gz ${JAPI_INSTALL}/icedtea6.japi.gz ; \
   ${JAPI_HOME}/bin/japicompat -v -j -o ${JAPI_INSTALL}/ignore-6-7.japio \
       ${JAPI_INSTALL}/icedtea6.japi.gz ${JAPI_INSTALL}/icedtea7.japi.gz ; \
+  ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/icedtea7-icedtea8.html \
+      ${JAPI_INSTALL}/icedtea7.japi.gz ${JAPI_INSTALL}/icedtea8.japi.gz ; \
+  ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/icedtea8-icedtea7.html \
+      ${JAPI_INSTALL}/icedtea8.japi.gz ${JAPI_INSTALL}/icedtea7.japi.gz ; \
+  ${JAPI_HOME}/bin/japicompat -v -j -o ${JAPI_INSTALL}/ignore-7-8.japio \
+      ${JAPI_INSTALL}/icedtea7.japi.gz ${JAPI_INSTALL}/icedtea8.japi.gz ; \
   ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/javadoc6-javadoc7.html \
       ${JAPI_INSTALL}/javadoc6.japi.gz ${JAPI_INSTALL}/javadoc7.japi.gz ; \
   ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/javadoc7-javadoc6.html \
       ${JAPI_INSTALL}/javadoc7.japi.gz ${JAPI_INSTALL}/javadoc6.japi.gz ; \
   ${JAPI_HOME}/bin/japicompat -v -j -o ${JAPI_INSTALL}/ignore-6-7-javadoc.japio \
-      ${JAPI_INSTALL}/javadoc6.japi.gz ${JAPI_INSTALL}/javadoc7.japi.gz ) 2>&1 | tee ${LOG_DIR}/japi-icedtea.errors && echo DONE
+      ${JAPI_INSTALL}/javadoc6.japi.gz ${JAPI_INSTALL}/javadoc7.japi.gz ; \
+  ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/javadoc7-javadoc8.html \
+      ${JAPI_INSTALL}/javadoc7.japi.gz ${JAPI_INSTALL}/javadoc8.japi.gz ; \
+  ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/javadoc8-javadoc7.html \
+      ${JAPI_INSTALL}/javadoc8.japi.gz ${JAPI_INSTALL}/javadoc7.japi.gz ; \
+  ${JAPI_HOME}/bin/japicompat -v -j -o ${JAPI_INSTALL}/ignore-7-8-javadoc.japio \
+      ${JAPI_INSTALL}/javadoc7.japi.gz ${JAPI_INSTALL}/javadoc8.japi.gz ) \
+  2>&1 | tee ${LOG_DIR}/japi-icedtea.errors && echo DONE
 fi
 
 if test "x${REBUILD_LIBGCJ_JAPI}" = "xyes"; then
@@ -45,8 +64,10 @@ if test "x${REBUILD_LIBGCJ_JAPI}" = "xyes"; then
   ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}-ibmjdk15.html ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}.japi.gz ${JAPI_INSTALL}/ibmjdk15.japi.gz ; \
   ${JAPI_HOME}/bin/japicompat -v -h -i ${JAPI_INSTALL}/ignore-6-7.japio -o ${JAPI_INSTALL}/icedtea6-libgcj-${SYSTEM_GCJ_VERSION}.html ${JAPI_INSTALL}/icedtea6.japi.gz ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}.japi.gz ; \
   ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}-icedtea6.html ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}.japi.gz ${JAPI_INSTALL}/icedtea6.japi.gz ; \
-  ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/icedtea7-libgcj-${SYSTEM_GCJ_VERSION}.html ${JAPI_INSTALL}/icedtea7.japi.gz ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}.japi.gz ; \
+  ${JAPI_HOME}/bin/japicompat -v -h -i ${JAPI_INSTALL}/ignore-7-8.japio -o ${JAPI_INSTALL}/icedtea7-libgcj-${SYSTEM_GCJ_VERSION}.html ${JAPI_INSTALL}/icedtea7.japi.gz ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}.japi.gz ; \
   ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}-icedtea7.html ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}.japi.gz ${JAPI_INSTALL}/icedtea7.japi.gz ; \
+  ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/icedtea8-libgcj-${SYSTEM_GCJ_VERSION}.html ${JAPI_INSTALL}/icedtea8.japi.gz ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}.japi.gz ; \
+  ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}-icedtea8.html ${JAPI_INSTALL}/libgcj-${SYSTEM_GCJ_VERSION}.japi.gz ${JAPI_INSTALL}/icedtea8.japi.gz ; \
   ) 2>&1 | tee ${LOG_DIR}/japi-libgcj.errors && echo DONE ;
 fi
 
@@ -60,13 +81,17 @@ ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/classpath-ibmjdk15.html ${J
 ${JAPI_HOME}/bin/japicompat -v -h -i ${JAPI_INSTALL}/ignore-6-7.japio -o ${JAPI_INSTALL}/icedtea6-classpath.html ${JAPI_INSTALL}/icedtea6.japi.gz \
     ${JAPI_INSTALL}/classpath.japi.gz ; \
 ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/classpath-icedtea6.html ${JAPI_INSTALL}/classpath.japi.gz ${JAPI_INSTALL}/icedtea6.japi.gz ; \
-${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/icedtea7-classpath.html ${JAPI_INSTALL}/icedtea7.japi.gz ${JAPI_INSTALL}/classpath.japi.gz ; \
+${JAPI_HOME}/bin/japicompat -v -h -i ${JAPI_INSTALL}/ignore-7-8.japio -o ${JAPI_INSTALL}/icedtea7-classpath.html ${JAPI_INSTALL}/icedtea7.japi.gz ${JAPI_INSTALL}/classpath.japi.gz ; \
 ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/classpath-icedtea7.html ${JAPI_INSTALL}/classpath.japi.gz ${JAPI_INSTALL}/icedtea7.japi.gz ; \
+${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/icedtea8-classpath.html ${JAPI_INSTALL}/icedtea8.japi.gz ${JAPI_INSTALL}/classpath.japi.gz ; \
+${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/classpath-icedtea8.html ${JAPI_INSTALL}/classpath.japi.gz ${JAPI_INSTALL}/icedtea8.japi.gz ; \
 ${JAPI_HOME}/bin/japicompat -v -h -i ${JAPI_INSTALL}/ignore-6-7-javadoc.japio -o ${JAPI_INSTALL}/gjdoc-javadoc6.html ${JAPI_INSTALL}/gjdoc.japi.gz \
     ${JAPI_INSTALL}/javadoc6.japi.gz ; \
 ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/javadoc6-gjdoc.html ${JAPI_INSTALL}/javadoc6.japi.gz ${JAPI_INSTALL}/gjdoc.japi.gz ; \
-${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/gjdoc-javadoc7.html ${JAPI_INSTALL}/gjdoc.japi.gz ${JAPI_INSTALL}/javadoc7.japi.gz ; \
+${JAPI_HOME}/bin/japicompat -v -h -i ${JAPI_INSTALL}/ignore-7-8-javadoc.japio -o ${JAPI_INSTALL}/gjdoc-javadoc7.html ${JAPI_INSTALL}/gjdoc.japi.gz ${JAPI_INSTALL}/javadoc7.japi.gz ; \
 ${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/javadoc7-gjdoc.html ${JAPI_INSTALL}/javadoc7.japi.gz ${JAPI_INSTALL}/gjdoc.japi.gz ; \
+${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/gjdoc-javadoc8.html ${JAPI_INSTALL}/gjdoc.japi.gz ${JAPI_INSTALL}/javadoc8.japi.gz ; \
+${JAPI_HOME}/bin/japicompat -v -h -o ${JAPI_INSTALL}/javadoc8-gjdoc.html ${JAPI_INSTALL}/javadoc8.japi.gz ${JAPI_INSTALL}/gjdoc.japi.gz ; \
 ) 2>&1 | tee ${LOG_DIR}/japi-compare.errors && echo DONE
 
 if test "x${JPEG_JAPI_COMPARISON}" = "xyes"; then
